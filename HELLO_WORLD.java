619d56980f373861ec781cdab3779be750dee857
unknown <manish.rana9@gmail.com>
2018-06-03 16:29:48 +0530

    hello world

---
+++ HELLO_WORLD.java
@@ -0,0 +1,12 @@
+
+public class HELLO_WORLD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		System.out.println("Hello Java World");
+		System.out.println("Hello Manish");
+
+	}
+
+}
